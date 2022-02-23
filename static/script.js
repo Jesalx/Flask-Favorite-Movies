b8b5e4b67db4eee5bbc0f4990a07caa3e4f326d1
Jesal Patel <jesalpatelx@gmail.com>
2022-02-23 16:11:57 -0500

    Consolidate javascript files

--- static/script.js
+++ static/script.js
@@ -1,3 +1,16 @@
+function meetsLoginConditions() {
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    let err_msg = document.getElementById("messages");
+
+    if (username.length === 0 || password.length === 0) {
+        err_msg.innerText = "Please fill in all fields.";
+        return false;
+    }
+
+    return true;
+}
+
 function meetsSignupConditions() {
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
